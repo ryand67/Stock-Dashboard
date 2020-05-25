23ec033011dd1f4cbe9504d07dd4f2e0236f0691
ryand67 <rwdevenney@gmail.com>
2020-05-24 22:26:29 -0400

    ajax call working

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
     let introForm = $("#introForm");
     let introSearchBtn = $("#introSearchBtn");
-    let searchSymbol;
+    let introTimeSelect = $("#introTimeSelect");
 
     introSearchBtn.on("click", function() {
         event.preventDefault();
-        searchSymbol = $("#introInput").val();
-        console.log(searchSymbol);
+        let stockName = $("#introInput").val().toUpperCase();
+        let timeInterval = introTimeSelect.val();
+        searchStock(stockName, timeInterval);
     })
+
+    const searchStock = (symbol, interval) => {
+        let apiURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=" + interval + "&datatype=json&apikey=00RBIHUDUPGDJNNH"
+        $.ajax({
+            url: apiURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response['Meta Data']);
+            
+        })
+    }
 })
